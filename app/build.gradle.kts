5f28d73c315c1a53db1fe2703a26d808a70d4ec9
XXX <XXX@qq.com>
2024-01-11 12:40:30 +0700

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+  alias(libs.plugins.androidApplication)
+  alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+  namespace = "com.cc.fits"
+  compileSdk = 34
+
+  defaultConfig {
+    applicationId = "com.cc.fits"
+    minSdk = 23
+    targetSdk = 34
+    versionCode = 1
+    versionName = "1.0"
+
+    vectorDrawables {
+      useSupportLibrary = true
+    }
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+  packaging {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+  }
+}
+
+dependencies {
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.appcompat)
+  implementation(libs.androidx.constraintlayout)
+  //implementation(libs.androidx.lifecycle.runtime.ktx)
+  //implementation(libs.androidx.material3)
+}
