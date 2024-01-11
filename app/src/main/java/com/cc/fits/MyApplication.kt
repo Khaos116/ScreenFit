e48cf5f7cb89f19520f38726ff61eda12c4d050e
XXX <XXX@qq.com>
2024-01-11 15:40:41 +0700

    代码优化

---
+++ app/src/main/java/com/cc/fits/MyApplication.kt
@@ -0,0 +1,14 @@
+package com.cc.fits
+
+import android.app.Application
+
+/**
+ * Author:Khaos116
+ * Date:2024/1/11
+ * Time:15:22
+ */
+class MyApplication : Application() {
+  override fun onCreate() {
+    super.onCreate()
+  }
+}
